12ecd51d8222b80345bbf5c10972b17062da159a
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-01-01 19:57:59 -0600

    Esconder elementos dependiendo de la ubicacion

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import './Nav.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 
 
 export default function Nav({ func }) {
-    return (
-        <nav>
-            <div className='linkBox'>
-                <Link to="home" className='enlaces'>Inicio</Link>
-                {/* <Link to="details" className='enlaces'>Detalles</Link> */}
-                <Link to="about" className='enlaces'>Acerca de</Link>
-            </div>
-            <SearchBar onSearch={func} />
-        </nav>
-    )
+    const location = useLocation();
+    console.log(location);
+    if (location.pathname !== '/') {
+        return (
+            <nav>
+                <div className='linkBox'>
+                    <Link to="home" className='enlaces'>Inicio</Link>
+                    <Link to="about" className='enlaces'>Acerca de</Link>
+                </div>
+                <SearchBar onSearch={func} />
+            </nav>
+        )
+    }
+
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import './SearchBar.css'
 
-export default function SearchBar({onSearch}) {
-   
+export default function SearchBar({ onSearch }) {
+
    const [nombre, setNombre] = useState('')
-   const handleInputChange=({target})=>{
+   const handleInputChange = ({ target }) => {
       setNombre(target.value)
    }
 
-
-   return (
-      <div className='divSearch'>         
-         <input type='search' placeholder='Nombre Personaje' onChange={handleInputChange}/>
-         <button onClick={()=>onSearch(nombre)}>Agregar</button>
-      </div>
-   );
+   const Location = useLocation()
+   if (Location.pathname === '/home') {
+      return (
+         <div className='divSearch'>
+            <input type='search' placeholder='Nombre Personaje' onChange={handleInputChange} />
+            <button onClick={() => onSearch(nombre)}>Agregar</button>
+         </div>
+      );
+   }
 }
