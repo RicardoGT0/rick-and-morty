db05ab6486923cbd18291c8f6d752c43a5912995
RicardoGT0 <rgonzaleztello@live.com.mx>
2023-01-17 23:31:03 -0600

    cambios de texto al about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ import "./About.css";
 export default function About() {
   return (
     <div className='divAbout'>
-      <h1 className='textabout'>Acerca del desarrollador</h1>
-      <h1 className='textabout'>Fecha de elaboracion: Diciembre 2022</h1>
+      <h1 className='textabout'>Fecha de elaboracion de la pagina: Diciembre 2022/Enero 2023</h1>
+      <h1 className='textabout'>Acerca del desarrollador</h1>      
       <h1 className='textabout'>Nombre: Ricardo González Tello</h1>
       <h1 className='textabout'>Titulo: M. en T. de Computo</h1>
       <div>
